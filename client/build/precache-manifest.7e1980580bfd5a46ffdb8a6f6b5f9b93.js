62c46395c4086a046008beeb35065d4e55e51a62
Leonard Poissant <leonardtati@gmail.com>
2021-03-27 14:45:03 -0400

    build

---
+++ client/build/precache-manifest.7e1980580bfd5a46ffdb8a6f6b5f9b93.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae7c1248279279cf0dbd97157da33ae1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b9a1a8e0a314793c7c3",
+    "url": "/static/css/main.7bfa597f.chunk.css"
+  },
+  {
+    "revision": "fcdc030169c1e5ab3b71",
+    "url": "/static/js/2.d907b80e.chunk.js"
+  },
+  {
+    "revision": "c8fad4de1da305ff34af4db9812b5d7f",
+    "url": "/static/js/2.d907b80e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b9a1a8e0a314793c7c3",
+    "url": "/static/js/main.a62c241b.chunk.js"
+  },
+  {
+    "revision": "103300903ff39ac4c13a",
+    "url": "/static/js/runtime-main.f806a36a.js"
+  },
+  {
+    "revision": "cefc20232703e5e3c24efd5f50d75e26",
+    "url": "/static/media/GitHub_Logo.cefc2023.png"
+  },
+  {
+    "revision": "8d21a89a23f6c7079de6529482b8156b",
+    "url": "/static/media/UnderConstruction.8d21a89a.jpg"
+  },
+  {
+    "revision": "5a862d8bcd9d8c130da2d64bbf8b8163",
+    "url": "/static/media/logo.5a862d8b.png"
+  }
+]);
